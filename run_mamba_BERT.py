e144b2b01dffa8b094495d64e999fbf0bbfa4006
davidaknowles <knowles84@gmail.com>
2024-03-16 23:40:34 +0000

    minor

--- run_mamba_BERT.py
+++ run_mamba_BERT.py
@@ -16,35 +16,36 @@ importlib.reload(tcn)
 
 pred_meta_task = True
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = tcn.MambaOneHotNet(in_channels = 6, out_channels = 5, n_embed = 64, n_layers = 8, receptive_field = 5000, bidir = True).to(device)
+
+checkpoint_path = Path("checkpoints_mamba_BERT")
+checkpoint_path.mkdir(exist_ok=True)
+
+optimizer = torch.optim.Adam(model.parameters())
+
+if True: # restart from last checkpoint
+    import glob
+    n_epoch = len(list(checkpoint_path.glob("*.pt")))
+    checkpoint = torch.load(checkpoint_path / ("%i.pt" % (n_epoch-1)))
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+
+# batch_size = 10. Cad done 2^20 ~ 1M tokens per batch. So og is 10x smaller
+
 get_gene = transcript_data.get_generator(
     os.path.expanduser("hg38.fa.gz"), 
     "gencode.v24.annotation.gtf.gz",
     "ENCFF191YXW.tsv.gz") # neural cell polyA RNA-seq
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-model = tcn.MambaOneHotNet(in_channels = 6, out_channels = 5, n_embed = 64, n_layers = 8, receptive_field = 5000, bidir = True).to(device)
-
 train_chroms = ["chr%i" % i for i in range(2,23)] + ["chrX"]
 test_chroms = ["chr1"]
 
-# batch_size = 10. Cad done 2^20 ~ 1M tokens per batch. So og is 10x smaller
-
 train_dataloader = transcript_data.get_dataloader(get_gene, train_chroms, receptive_field = 5000, batch_size = 3, device = device, max_len = 10000 )
 test_dataloader = transcript_data.get_dataloader(get_gene, test_chroms, receptive_field = 5000, batch_size = 1, device = device, max_len = 30000 )
-
-optimizer = torch.optim.Adam(model.parameters())
-
-checkpoint_path = Path("checkpoints_mamba_BERT")
-checkpoint_path.mkdir(exist_ok=True)
-
-if False: # restart from last checkpoint
-    import glob
-    n_epoch = len(glob.glob(checkpoint_path / "*.pt"))
-    checkpoint = torch.load(checkpoint_path / ("%i.pt" % (n_epoch-1)))
-    model.load_state_dict(checkpoint['model_state_dict'])
-    model.to(device)
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
 for epoch in range(100): #  range(n_epoch, n_epoch + 40): 
     np.random.seed(int(time.time()))
